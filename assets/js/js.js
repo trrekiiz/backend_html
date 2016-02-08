65dd254b7742d215bec5796a390c45ddb9adbf80
trrekiiz <trrekiiz@camphub.in.th>
2016-02-08 09:24:29 +0700

    change css

--- assets/js/js.js
+++ assets/js/js.js
@@ -26,7 +26,7 @@ function generatePagination () {
 }
 
 function goToNextSlide () {
-  if(currentSlide >= slideCount - 1) return; 
+  if(currentSlide >= slideCount - 1) return;
   var windowWidth = $(window).width();
   currentSlide++;
   $slideContainer.animate({
@@ -39,7 +39,7 @@ function goToNextSlide () {
 }
 
 function goToPreviousSlide () {
-  if(currentSlide <= 0) return; 
+  if(currentSlide <= 0) return;
   var windowWidth = $(window).width();
   currentSlide--;
   $slideContainer.animate({
@@ -73,3 +73,50 @@ generatePagination();
 $(window).resize(postitionSlides);
 $('.next').on('click', goToNextSlide);
 $('.previous').on('click', goToPreviousSlide);
+
+
+// form
+
+// Just for the selectbox
+
+$(document).ready(function(){
+  simpleSelect();
+});
+
+function simpleSelect() {
+  "use strict";
+  var selectHolder,
+      selectClass;
+  //Setup
+  $('select').each(function() {
+    selectClass = $(this).attr('class');
+    selectHolder = '<dl class="simpleSelect '+selectClass+'">';
+    selectHolder += '<dt>'+$('option', this).first().text()+'</dt><dd><ul>';
+    $('option', this).each(function() {
+      selectHolder += '<li data="'+$(this).val()+'">'+$(this).text()+'</li>';
+    });
+    selectHolder += '</ul></dd></dl>';
+    $(this).after(selectHolder);
+    $('.'+selectClass).wrapAll('<div class="selectContainer"></div>');
+  });
+
+  //Clicks
+  $('.simpleSelect dd ul li').on("click",function(){
+    $(this).parents().eq(3).find('select').val($(this).attr('data'));
+  });
+
+  $('.simpleSelect dt').on("click",function() {
+    if($(this).next('dd').hasClass("open")){
+      $(this).removeClass("open").next('dd').removeClass("open");
+    }
+    else {
+      $(this).addClass("open").next('dd').addClass("open");
+    }
+  });
+
+  $('.simpleSelect dd ul li').on("click",function() {
+    $(this).parents().eq(1).removeClass("open");
+    $(this).parents().eq(2).find('dt').removeClass("open");
+    $(this).parents().eq(4).find('dt').text($(this).text());
+  });
+}
